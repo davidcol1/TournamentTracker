6c6ee9d30520f10030ea47b53d2dd236ac805ee7
david colvin <davidmcolvin@gmail.com>
2019-11-03 22:42:07 -0500

    Add Sql set up scripts

---
+++ TrackerLibrary/SqlScripts/01-TournamentTrackerCreateTablesScript.sql
@@ -0,0 +1,80 @@
+use Tournaments;
+go
+
+create table Teams (
+id int identity (1,1) not null primary key,
+TeamName varchar(100) not null
+);
+go
+
+create table People (
+id int identity (1,1) not null primary key,
+FirstName varchar(100) not null,
+LastName varchar(100) not null,
+EmailAddress varchar(100) not null,
+CellphoneNumber varchar(100) not null
+);
+go
+
+create table TeamMembers (
+id int identity (1,1) not null primary key,
+TeamId int not null,
+PersonId int not null,
+foreign key (TeamId) references Teams(id),
+foreign key (PersonId) references People(id)
+);
+go
+
+create table Matchups (
+id int identity (1,1) not null primary key,
+WinnerId int not null,
+MatchupRound int not null,
+foreign key (WinnerId) references Teams(id)
+);
+go
+
+create table MatchupEntries (
+id int identity (1,1) not null primary key,
+MatchupId int not null,
+ParentMatchupId int not null,
+TeamCompeteingId int not null,
+Score int not null,
+foreign key (MatchupId) references Matchups (id),
+foreign key (ParentMatchupId) references Matchups (id),
+foreign key (TeamCompeteingId) references Teams (id)
+);
+go
+
+create table Tournaments (
+id int identity (1,1) not null primary key,
+TournamentName varchar(100) not null,
+EntryFee money not null
+);
+go
+
+create table TournamentEntries (
+id int identity (1,1) not null primary key,
+TournamentId int not null,
+TeamId int not null,
+foreign key (TournamentId) references Tournaments (id),
+foreign key (TeamId) references Teams (id)
+);
+go
+
+create table Prizes (
+id int identity (1,1) not null primary key,
+PlaceNumber int not null,
+PlaceName varchar(100) not null,
+PrizeAmount money not null default 0,
+PrizePercentage float not null default 0
+);
+go
+
+create table TournamentPrizes (
+id int identity (1,1) not null primary key,
+TournamentId int not null,
+PrizeId int not null,
+foreign key (TournamentId) references Tournaments (id),
+foreign key (PrizeId) references Prizes (id)
+);
+go
---
+++ TrackerLibrary/SqlScripts/02-TournamentTrackerCreateStoredProcsScript.sql
@@ -0,0 +1,146 @@
+if object_id('spMatchupEntries_GetByMatchup', 'P') is not null
+  drop proc spMatchupEntries_GetByMatchup
+go
+
+create procedure spMatchupEntries_GetByMatchup
+  @MatchupId int
+as
+begin
+  set nocount on
+
+  select *
+  from MatchupEntries me
+  where me.MatchupId = @MatchupId
+
+end
+go
+
+if object_id('spMatchups_GetByTournament', 'P') is not null
+  drop proc spMatchups_GetByTournament
+go
+
+create procedure spMatchups_GetByTournament
+  @TournamentId int
+as
+begin
+  set nocount on
+
+  select m.*
+  from Matchups m
+  inner join MatchupEntries me
+  on me.MatchupId = m.id
+  inner join Teams t
+  on t.id = me.TeamCompeteingId
+  inner join TournamentEntries te
+  on te.TeamId = t.Id
+  and te.TournamentId = @TournamentId
+
+end
+go
+
+if object_id('spPeople_GetAll', 'P') is not null
+  drop proc spPeople_GetAll
+go
+
+create procedure spPeople_GetAll
+as
+begin
+  set nocount on
+
+  select *
+  from People
+
+end
+go
+
+
+if object_id('spPrizes_GetByTournament', 'P') is not null
+  drop proc spPrizes_GetByTournament
+go
+
+create procedure spPrizes_GetByTournament
+  @TournamentId int
+as
+begin
+  set nocount on
+
+  select p.*
+  from Prizes p
+  inner join TournamentPrizes tp
+  on tp.TournamentId = @TournamentId
+
+end
+go
+
+
+if object_id('spTeams_GetByTournament', 'P') is not null
+  drop proc spTeams_GetByTournament
+go
+create procedure spTeams_GetByTournament
+  @TournamentId int
+as
+begin
+  set nocount on
+
+  select t.*
+  from Teams t
+  inner join TournamentEntries te
+  on te.TeamId = t.Id
+  and te.TournamentId = @TournamentId
+
+end
+go
+
+if object_id('spTeamMembers_GetByTeam', 'P') is not null
+  drop proc spTeamMembers_GetByTeam
+go
+create procedure spTeamMembers_GetByTeam
+  @TeamId int
+as
+begin
+  set nocount on
+
+  select te.*
+  from Teams t
+  inner join TeamMembers te
+  on te.TeamId = t.Id
+  where t.id = @TeamId
+
+end
+go
+
+if object_id('spTournaments_GetAll', 'P') is not null
+  drop proc spTournaments_GetAll
+go
+
+create procedure spTournaments_GetAll
+as
+begin
+  set nocount on
+
+  select *
+  from Tournaments
+
+end
+go
+
+if object_id('spPrizes_Insert', 'P') is not null
+  drop proc spPrizes_Insert
+go
+
+create procedure spPrizes_Insert
+  @PlaceNumber int,
+  @PlaceName varchar(100),
+  @PrizeAmount money,
+  @PrizePercentage float,
+  @id int = 0 output
+as
+begin
+  set nocount on
+
+  insert into Prizes (PlaceNumber, PlaceName, PrizeAmount, PrizePercentage)
+  values (@PlaceNumber, @PlaceName, @PrizeAmount, @PrizePercentage)
+
+  select @id = SCOPE_IDENTITY();
+end
+go
